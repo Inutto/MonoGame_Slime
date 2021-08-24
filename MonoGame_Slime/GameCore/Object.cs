4ca12908f8d60afd76064a67383c4bde9e8c7dcc
Inutto <57230511+Inutto@users.noreply.github.com>
2021-08-24 16:42:56 -0600

    add World and Object

---
+++ MonoGame_Slime/GameCore/Object.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+
+namespace MonoGame_Slime.GameCore
+{
+    class Object
+    {
+        protected Texture2D image;
+        protected Color color = Color.White;
+
+        public Vector2 position;
+        public Vector2 velocity; // for slime
+
+
+        public virtual void Update()
+        {
+            
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the sprite
+        }
+
+    }
+}
---
+++ MonoGame_Slime/GameCore/World.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace MonoGame_Slime.GameCore
+{
+    /// <summary>
+    /// The container of all game objects. Can only created by once. (Singleton)
+    /// </summary>
+    class World
+    {
+        // Rotation
+        public Vector2 center;          // the rotation center, also the center of the world
+        public float direction;         // the global orientation of the world
+
+        // Child
+        public List<Object> objectList; // store all the objects
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+    }
+}
