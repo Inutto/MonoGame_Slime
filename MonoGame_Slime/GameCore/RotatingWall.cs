5c05ed342738c93280a1c609f914302b2d353ef0
Inutto <57230511+Inutto@users.noreply.github.com>
2021-08-30 12:54:06 -0600

    Maybe end of day I have a meeting to attedn now

--- MonoGame_Slime/GameCore/RotatingWall.cs
+++ MonoGame_Slime/GameCore/RotatingWall.cs
@@ -9,7 +9,7 @@ namespace MonoGame_Slime.GameCore
     public class RotatingWall : Wall
     {
 
-        public float rotationSpeed = -0.05f;
+        public float rotationSpeed = -0.07f;
 
         public RotatingWall(Vector2 _centerPosition, Vector2 _size, float _rotation = 0f) : base(_centerPosition, _size, _rotation)
         {
